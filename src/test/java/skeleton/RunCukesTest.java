69640475e20620fb61f902af64266298416c297a
pavanvarma2923 <54269091+pavanvarma2923@users.noreply.github.com>
2019-08-23 14:35:20 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,11 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="",glue="skeleton",tags= {"@Sanity"},plugin= {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+@CucumberOptions(features="resources",glue="skeleton",tags= {"@Sanity"},plugin= {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
 
-
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"json:target/cucumber.json"})
 public class RunCukesTest {
 }
